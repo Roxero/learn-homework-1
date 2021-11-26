d004aee06484ad8f757bacf2b8289c70e74998b8
Roxero <56275207+Roxero@users.noreply.github.com>
2021-11-26 18:15:59 +0300

    Update 8_ephem_bot.py

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -21,11 +21,7 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=loggi
 
 def greet_user(update, context):
 	# getting user tg name
-<<<<<<< HEAD
 	user_name = []
-=======
-	user_name = list()
->>>>>>> 7b1c9e40ed51e4fd47ff6c4d3f06ec703dbf72e3
 	chat_data = update['message']['chat']
 	if chat_data['first_name']:
 		user_name.append(chat_data['first_name'])
@@ -49,52 +45,10 @@ def planet_constellation(update, context):
 	if len(user_text) == 2:
 		planet = None # avoid reference before assignment err
 		planet_name = user_text[1].lower().capitalize()
-<<<<<<< HEAD
 		if hasattr(ephem, planet_name):
 			planet = getattr(ephem, planet_name)()
-			"""
-			if planet_name == 'Mercury':
-				planet = ephem.Mercury()
-			elif planet_name == 'Venus':
-				planet = ephem.Venus()
-			elif planet_name == 'Mars':
-				planet = ephem.Mars()
-			elif planet_name == 'Jupiter':
-				planet = ephem.Jupiter()
-			elif planet_name == 'Saturn':
-				planet = ephem.Saturn()
-			elif planet_name == 'Neptune':
-				planet = ephem.Neptune()
-			elif planet_name == 'Uranus':
-				planet = ephem.Uranus()
-			elif planet_name == 'Pluto':
-				planet = ephem.Pluto()
-			else:
-				update.message.reply_text('Не знаю такой планеты')
-			"""
 		else:
 			update.message.reply_text('Не знаю такой планеты')
-		
-=======
-		if planet_name == 'Mercury':
-			planet = ephem.Mercury()
-		elif planet_name == 'Venus':
-			planet = ephem.Venus()
-		elif planet_name == 'Mars':
-			planet = ephem.Mars()
-		elif planet_name == 'Jupiter':
-			planet = ephem.Jupiter()
-		elif planet_name == 'Saturn':
-			planet = ephem.Saturn()
-		elif planet_name == 'Neptune':
-			planet = ephem.Neptune()
-		elif planet_name == 'Uranus':
-			planet = ephem.Uranus()
-		elif planet_name == 'Pluto':
-			planet = ephem.Pluto()
-		else:
-			update.message.reply_text('Не знаю такой планеты')
->>>>>>> 7b1c9e40ed51e4fd47ff6c4d3f06ec703dbf72e3
 		planet.compute()
 		const = ephem.constellation(planet)[1]
 		update.message.reply_text(f'Планета {planet_name} находится сегодня в созвездии {const}')
@@ -102,13 +56,7 @@ def planet_constellation(update, context):
 		update.message.reply_text('Введи: /planet Planetname')
 
 def main():
-<<<<<<< HEAD
-	#mybot = Updater('BOTHFATHER_BOT_TOKEN', use_context=True)
-	mybot = Updater('2105769211:AAEc1JV2ZS9jA8arrRHyHX7z-zXI_umkf70', use_context=True)
-	
-=======
 	mybot = Updater('BOTHFATHER_BOT_TOKEN', use_context=True)
->>>>>>> 7b1c9e40ed51e4fd47ff6c4d3f06ec703dbf72e3
 	dp = mybot.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
 	dp.add_handler(CommandHandler("planet", planet_constellation))
